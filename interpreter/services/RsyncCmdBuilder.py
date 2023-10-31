1dd7dd2b28cd6905b720f6f8accbaffa67803611
vitalii <vitaly.i.dev@gmail.com>
2023-10-31 21:24:38 +0100

    fix remote rsync issue with unknown host

--- interpreter/services/RsyncCmdBuilder.py
+++ interpreter/services/RsyncCmdBuilder.py
@@ -50,13 +50,11 @@ class RsyncCmdBuilder:
         return self
 
     def dest_folder(self, path):
-
+        self._rsync_cmd.append('.')
         if self._dest_ssh_prefix:
-            self._rsync_cmd.append(f"-e '\"ssh -o StrictHostKeyChecking=no\"'")
-            self._rsync_cmd.append('.')
+            self._cmd_list.append(' '.join("ssh -o StrictHostKeyChecking=no"))
             self._rsync_cmd.append(f'{self._dest_ssh_prefix}:{path}')
         else:
-            self._rsync_cmd.append('.')
             self._rsync_cmd.append(f'{path}')
         self._cmd_list.append(' '.join(self._rsync_cmd))
         return self
